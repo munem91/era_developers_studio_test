4e0f5f627e1497bba18321ca3ef5613b53ab9158
munem91 <borisukrusaln12345@gmail.com>
2024-05-29 16:56:46 +0530

    init

--- lib/repositories/news/abstract_news_repository.dart
+++ lib/repositories/news/abstract_news_repository.dart
@@ -1,7 +1,20 @@
-import 'package:forestvpn_test/repositories/news/repository.dart';
+import 'package:era_developers_test_flutter/repositories/news/models/models.dart';
 
+
+/// Abstract class for the News Repository. It provides the necessary methods
+/// to get latest articles, featured articles and a specific article.
+///
+/// The class is abstract, so it can't be instantiated. It's meant to be extended
+/// by a concrete implementation of the repository.
 abstract class AbstractNewsRepository {
+  
+  /// Returns a list of the latest articles.
   Future<List<Article>> getLatestArticles();
+  
+  /// Returns a list of the featured articles.
   Future<List<Article>> getFeaturedArticles();
+  
+  /// Returns the article with the specified [id].
   Future<Article> getArticle(String id);
 }
+
--- lib/repositories/news/models/article.dart
+++ lib/repositories/news/models/article.dart
@@ -15,3 +15,5 @@ class Article {
   final bool readed;
   final String? description;
 }
+
+
